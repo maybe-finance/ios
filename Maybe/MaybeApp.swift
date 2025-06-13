cb0ff21e014bc55e651e18d49601f3b234f8001a
Josh Pigford <josh@joshpigford.com>
2025-06-13 09:07:10 -0500

    Initial Commit

---
+++ Maybe/MaybeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaybeApp.swift
+//  Maybe
+//
+//  Created by Josh Pigford on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct MaybeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
